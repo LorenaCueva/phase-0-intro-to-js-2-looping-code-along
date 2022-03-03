404467f9ca006dca309e43e5aa0b31a394a165ec
Lorena Cueva <lorenacueva@gmail.com>
2022-03-03 13:38:52 -0800

    commit tree 97a9efdbc672781128dc7de8f69fc5c59473d071

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, message){
+    let finishedCards = [];
+    for(let i = 0; i < names.length; i++){
+        finishedCards.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`)
+        debugger;
+    }
+    return finishedCards;
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num--);
+    }
+}
